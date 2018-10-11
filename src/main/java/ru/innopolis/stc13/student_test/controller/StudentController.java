d4a5f7b7d82b9660b0a0af700581b21911f9318f
maks <maxim_86nva@mail.ru>
2018-10-11 23:02:33 +0300

    added ability to delete of students from list

--- src/main/java/ru/innopolis/stc13/student_test/controller/StudentController.java
+++ src/main/java/ru/innopolis/stc13/student_test/controller/StudentController.java
@@ -3,6 +3,8 @@ package ru.innopolis.stc13.student_test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.innopolis.stc13.student_test.pojo.Student;
 import ru.innopolis.stc13.student_test.service.StudentService;
@@ -10,6 +12,7 @@ import ru.innopolis.stc13.student_test.service.StudentService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/students")
 public class StudentController {
 
     private StudentService studentService;
@@ -19,10 +22,16 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @RequestMapping("/students")
-    public String getBrands(Model model) {
+    @GetMapping
+    public String getStudents(Model model) {
         List<Student> list = studentService.getAll();
         model.addAttribute("students", list);
         return "students";
+    }
+
+    @GetMapping("/remove/{id}")
+    public String removeBook(@PathVariable("id") int id){
+        this.studentService.delete(id);
+        return "redirect:/students";
     }
 }
