f45620200f5b7db98358e98bc310eea5a24015ac
maks <maxim_86nva@mail.ru>
2018-10-15 20:41:29 +0300

    added role, merge teacher, student, admin in User, remove teacher, student, admin

--- src/main/java/ru/innopolis/stc13/student_test/controller/TeacherController.java
+++ src/main/java/ru/innopolis/stc13/student_test/controller/TeacherController.java
@@ -3,7 +3,7 @@ package ru.innopolis.stc13.student_test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.innopolis.stc13.student_test.pojo.Teacher;
 import ru.innopolis.stc13.student_test.service.TeacherService;
 
