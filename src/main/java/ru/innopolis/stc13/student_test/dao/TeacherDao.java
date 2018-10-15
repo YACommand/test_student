8f4b1cd28a845a7dd1ab84050d8a7637bcbe2774
maks <maxim_86nva@mail.ru>
2018-10-15 23:16:31 +0300

    YAfix

--- src/main/java/ru/innopolis/stc13/student_test/dao/TeacherDao.java
+++ src/main/java/ru/innopolis/stc13/student_test/dao/TeacherDao.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.innopolis.stc13.student_test.pojo.Teacher;
 
 public interface TeacherDao extends JpaRepository<Teacher, Integer> {
+
 }
