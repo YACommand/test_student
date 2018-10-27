898d8359e741a6bb6b831b9ac5abb0174064b449
maks <maxim_86nva@mail.ru>
2018-10-27 20:08:16 +0300

    Merge branch 'develop' of /Users/maksim/IdeaProjects/test_student with conflicts.

---
+++ src/main/java/ru/innopolis/stc13/student_test/config/WebSecurityConfig.java
@@ -0,0 +1,84 @@
+package ru.innopolis.stc13.student_test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+
+        // включаем защиту от CSRF атак
+        http.csrf()
+                .disable()
+                // указываем правила запросов
+                // по которым будет определятся доступ к ресурсам и остальным данным
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/**").permitAll()
+                .anyRequest().permitAll()
+                .and();
+
+        http.formLogin()
+                // указываем страницу с формой логина
+                .loginPage("/login")
+                // указываем action с формы логина
+                .loginProcessingUrl("/j_spring_security_check")
+                // указываем URL при неудачном логине
+                .failureUrl("/login?error")
+                // Указываем параметры логина и пароля с формы логина
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                // даем доступ к форме логина всем
+                .permitAll();
+
+        http.logout()
+                // разрешаем делать логаут всем
+                .permitAll()
+                // указываем URL логаута
+                .logoutUrl("/logout")
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login?logout")
+                // делаем не валидной текущую сессию
+                .invalidateHttpSession(true);
+
+    }
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select login, password from users where login=?")
+                .authoritiesByUsernameQuery("select ul.login, ur.roles from users ul inner join roles ur on ul.id=ur.user_id where ul.login=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/login.jsp
@@ -0,0 +1,18 @@
+<%@include file="header-bootstrap.jsp" %>
+<%@include file="header-bootstrap.jsp" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<body>
+<div class="container" style="width: 300px;">
+    <c:url value="/j_spring_security_check" var="loginUrl" />
+    <form action="${loginUrl}" method="post">
+        <h2 class="form-signin-heading">Sign in</h2>
+        <input type="text" class="form-control" name="j_username" placeholder="Your login">
+        <input type="password" class="form-control" name="j_password" placeholder="Password">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+    </form>
+</div>
+</body>
+<%@include file="footer-bootstrap.jsp" %>
