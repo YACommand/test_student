0ec2814b6cea7a3c75c6dc949390db3b5ad92372
Grisha Sostavkin <grishalarin@mail.ru>
2018-10-27 17:50:41 +0300

    security

---
+++ src/main/java/ru/innopolis/stc13/student_test/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package ru.innopolis.stc13.student_test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select login, password from users where login=?")
+                .authoritiesByUsernameQuery("select ul.login, ur.roles from users ul inner join roles ur on ul.id=ur.user_id where ul.login=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        String passsword2 = "5";
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(passsword2);
+
+        System.out.println(hashedPassword);
+    }
+}
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -4,13 +4,13 @@
 <main role="main" class="container">
 
     <ul class="list-group-item-dark m-2">
-    <li><a href="/users/students">Students</a></li>
-    <li><a href="/users/teachers">Teachers</a></li>
-    <li><a href="/users/admins">Admins</a></li>
+        <li><a href="/users/students">Students</a></li>
+        <li><a href="/users/teachers">Teachers</a></li>
+        <li><a href="/users/admins">Admins</a></li>
         <li><a href="/tests">Tests</a></li>
-      <li><a href="/users/admins">Admins</a></li>
-    <li><a href="/groups">Groups</a></li>
-</ul>
+        <li><a href="/users/admins">Admins</a></li>
+        <li><a href="/groups">Groups</a></li>
+    </ul>
 </text>
 </html>
 </main>
