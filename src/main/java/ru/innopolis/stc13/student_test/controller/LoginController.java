2eebb4741cc9a06665f46fb52b8be89c63c4ab68
maks <maxim_86nva@mail.ru>
2018-10-27 18:40:34 +0300

    task 1 - 18: added selection of groups for the student

--- src/main/java/ru/innopolis/stc13/student_test/controller/LoginController.java
+++ src/main/java/ru/innopolis/stc13/student_test/controller/LoginController.java
@@ -1,30 +1,18 @@
 package ru.innopolis.stc13.student_test.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(Model model){
-
-
+    @RequestMapping(method = RequestMethod.GET)
+    public String loginPage(Model model){
         return "login";
     }
-
-//    @RequestMapping(value = "/login", method = RequestMethod.GET)
-//    public String loginPage(@RequestParam(value = "error", required = false) String error,
-//                            @RequestParam(value = "logout", required = false) String logout,
-//                            Model model) {
-//        String errorMessge = null;
-//        if(error != null) {
-//            errorMessge = "Username or Password is incorrect !!";
-//        }
-//        if(logout != null) {
-//            errorMessge = "You have been successfully logged out !!";
-//        }
-//        model.addAttribute("errorMessge", errorMessge);
-//        return "login";
-//    }
 
 }
